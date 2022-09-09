205e9f4c1185b662d589438fc66f033e05785cd7
Taiwola <seunolanitori@gmail.com>
2022-09-09 14:06:12 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main - entry points
- * Return: 0 when successful
+ * Return: 1 when successful
  */
 int main(void)
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+{write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
