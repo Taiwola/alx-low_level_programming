fe88524adbbdbc83866a53a304f63c9a80a92024
Taiwola <seunolanitori@gmail.com>
2022-09-08 20:14:08 -0700

    04. puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
+ *
  * return: always 0
  */
 int main(void)
 {puts("\"programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
