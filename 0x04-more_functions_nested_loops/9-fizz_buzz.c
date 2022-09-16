fb895eb74b3ffaf66b610b8df82958a52528b274
Taiwola <seunolanitori@gmail.com>
2022-09-15 23:17:03 -0700

    9. fizzbuzzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int = 1;
+
+	while (i++ < 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("fizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf('\n');
+	return (0);
+}
