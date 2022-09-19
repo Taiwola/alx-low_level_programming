0ae700c586fa45fe9d79c38c222c4cadc0fefb96
Taiwola <seunolanitori@gmail.com>
2022-09-19 13:47:50 -0700

    02 length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - print the klenght of a string
+ * @n: string variable
+ * Return: lenght of the string
+ */
+
+int _strlen(char *n)
+{
+	return (strlen(n));
+}
