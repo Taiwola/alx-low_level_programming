cfbbcc977dc44659c821e8a8ddb19e24096e4fef
Taiwola <seunolanitori@gmail.com>
2022-09-21 17:06:36 -0700

    concatenates two string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-
+	while (src[i] != '\0' && i < n)
+	{
 		dest[i] = src[i];
-
-	for (; i < n; i++)
-
+		i++;
+	}
+	while (i < n)
+	{
 		dest[i] = '\0';
-
+		i++;
+	}
 	return (dest);
 }
