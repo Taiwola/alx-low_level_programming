d74f7f3fa139acc64ff8c29be0a7c1ace7192fc5
Taiwola <seunolanitori@gmail.com>
2022-10-04 20:24:14 -0700

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
  * Return: return NULL if either size is zero or fails
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *array_alloc = malloc(size * sizeof(char));
 	unsigned int i;
